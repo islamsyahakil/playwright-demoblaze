99026cccc193588239e3221c6b0061dccb7c8cc9
islamsyahakil <islamsyah.akil@gmail.com>
2025-09-06 21:50:45 +0700

    fixing hook.cjs

--- features/support/hooks.cjs
+++ features/support/hooks.cjs
@@ -1,16 +1,3 @@
-const { Before, After } = require('@cucumber/cucumber');
-const { chromium } = require('playwright');
-
-Before(async function () {
-  this.browser = await chromium.launch({ headless: false });
-  this.context = await this.browser.newContext();
-  this.page = await this.context.newPage();
-});
-
-After(async function () {
-  await this.context?.close();
-  await this.browser?.close();
-});
 const { BeforeAll, AfterAll, Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
 const { chromium } = require('playwright');
 
